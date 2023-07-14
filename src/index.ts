4ad94a3a4ffa3efc287791a215efab21315a4c4c
Thanh Nguyen <nguyen.hathanh0810@gmail.com>
2023-07-14 17:07:42 +0700

    feat: serve with mongoDB connected

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,34 @@
-export function sayHelloWorld(world: string) {
-  return `Hello ${world}`;
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+const mongodbURL = `${process.env.MONGODB_URL}`;
+
+app.get("/", (_: Request, res: Response) => {
+  res.send("Coupons app with Express & Typescript 🎫");
+});
+
+async function connectDB(callback: Function) {
+  mongoose.connect(mongodbURL).then(
+    () => {
+      console.log("🔌[mongodb]: MongoDB is connected successfully.");
+      if (callback) {
+        callback();
+      }
+    },
+    (error: Error) => {
+      throw error;
+    }
+  )
 }
+connectDB(() => {
+  app.listen(port, () => {
+    console.log(`⚡[server]: Server is running at //localhost:${port}.`);
+  });
+});
+
+export { app as server };
